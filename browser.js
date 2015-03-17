944ba2a4957768cba2957aaf9ecbef98f0041324
Ritchie Martori <skawful@gmail.com>
2015-03-17 13:42:06 -0700

    first commit

---
+++ browser.js
@@ -0,0 +1,55 @@
+var Client = require('strong-pubsub');
+var Adapter = require('strong-pubsub-mqtt');
+var duplex = require('duplex');
+
+Primus.Stream = require('stream');
+
+module.exports = function(PORT) {
+  var client = new Client({port: PORT, host: 'localhost'}, Adapter, {
+    createConnection: function(port, host) {
+      var connection = duplex();
+      var primus = Primus.connect('http://' + host + ':' + port, {
+        transformer: 'engine.io',
+        parser: 'binary'
+      });
+
+      connection.on('_data', function(chunk) {
+        // someone called `connection.write(buf)`
+        primus.write(chunk);
+      });
+
+      primus.on('data', function(chunk) {
+        // chunk is an arrayBuffer
+        connection._data(toBuffer(chunk));
+      });
+
+      primus.on('open', function() {
+        connection.emit('connect');
+      });
+
+      connection.on('_end', function() {
+        primus.end();
+        this._end();
+      });
+
+      return connection;
+    }
+  });
+
+  client.subscribe('foo', function() {
+    console.log('subscribed');
+  });
+  client.on('message', function(topic, msg) {
+    console.log(topic, msg.toString());
+  });
+}
+
+
+function toBuffer(ab) {
+  var buffer = new Buffer(ab.byteLength);
+  var view = new Uint8Array(ab);
+  for(var i = 0; i < buffer.length; ++i) {
+    buffer[i] = view[i];
+  }
+  return buffer;
+}
